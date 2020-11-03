541cffd6527b8d8a1d0faaf41985619ce70988f9
mei15 <marimarpubg@gmail.com>
2020-11-04 00:10:02 +0700

    ubah api

--- lib/screens/konsultasi/detail.dart
+++ lib/screens/konsultasi/detail.dart
@@ -64,63 +64,64 @@ class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("${widget.list[widget.index]['judul']}"),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: new Container(
-        height: 270.0,
-        padding: const EdgeInsets.all(20.0),
-        child: new Card(
-          child: new Column(
-            children: <Widget>[
-              new Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-              ),
-              new Text(
-                "Judul : ${widget.list[widget.index]['judul']}",
-                style: new TextStyle(fontSize: 20.0),
-              ),
-              new Text(
-                "Tanggal : ${widget.list[widget.index]['tanggal']}",
-                style: new TextStyle(fontSize: 18.0),
-              ),
-              new Text(
-                "Keterangan : ${widget.list[widget.index]['keterangan']}",
-                style: new TextStyle(fontSize: 18.0),
-              ),
-              new Text(
-                "Dosen : ${widget.list[widget.index]['dosen']['first_name'] + " " + widget.list[widget.index]['dosen']['last_name']}",
-                style: new TextStyle(fontSize: 18.0),
-              ),
-              new Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-              ),
-              new Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  new RaisedButton(
-                    child: new Text("EDIT"),
-                    color: Colors.green,
-                    onPressed: () =>
-                        Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext context) => new EditData(
-                        list: widget.list,
-                        index: widget.index,
-                      ),
-                    )),
-                  ),
-                  new RaisedButton(
-                    child: new Text("DELETE"),
-                    color: Colors.red,
-                    onPressed: () => confirm(),
-                  ),
-                ],
-              )
-            ],
-          ),
+        appBar: new AppBar(
+          title: new Text("${widget.list[widget.index]['judul']}"),
+          backgroundColor: Colors.blueGrey,
         ),
-      ),
-    );
+        body: new Container(
+          height: 270.0,
+          padding: const EdgeInsets.all(20.0),
+          child: new Card(
+            child: Center(
+              child: new Column(
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                  ),
+                  new Text(
+                    "Judul : ${widget.list[widget.index]['judul']}",
+                    style: new TextStyle(fontSize: 20.0),
+                  ),
+                  new Text(
+                    "Tanggal : ${widget.list[widget.index]['tanggal']}",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                  new Text(
+                    "Keterangan : ${widget.list[widget.index]['keterangan']}",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                  new Text(
+                    "Dosen : ${widget.list[widget.index]['dosen']['first_name'] + " " + widget.list[widget.index]['dosen']['last_name']}",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                  ),
+                  new Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new RaisedButton(
+                        child: new Text("EDIT"),
+                        color: Colors.green,
+                        onPressed: () =>
+                            Navigator.of(context).push(new MaterialPageRoute(
+                          builder: (BuildContext context) => new EditData(
+                            list: widget.list,
+                            index: widget.index,
+                          ),
+                        )),
+                      ),
+                      new RaisedButton(
+                        child: new Text("DELETE"),
+                        color: Colors.red,
+                        onPressed: () => confirm(),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
